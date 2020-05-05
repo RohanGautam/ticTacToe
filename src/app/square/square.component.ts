784491c4963add1883d85eda70a9b620f43541e3
RohanGautam <rohan1gautam@gmail.com>
2020-05-05 15:12:23 +0800

    stateless component

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -13,4 +13,6 @@ export class SquareComponent {
   // use : <app-square value="X"></app-square> in the parent html
   @Input() value :'X'|'O'; // a value field that can be taken from parent. can only be x or o. 
 
+  // Dumb or Stateless component. It cannot modify it's own state. It just shows what i gets from it's parent.
+
 }
